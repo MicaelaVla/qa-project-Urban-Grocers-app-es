a94475818456f3eff6b6d3a39098dea3a65a0397
MicaelaVla <micaela1993arg@gmail.com>
2024-06-08 21:25:08 -0300

    proyect finished2

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = ("https://cnt-3a48f5a7-d062-41ff-bf12-fcef27b7ac8c.containerhub.tripleten-services.com")
+URL_SERVICE = ("https://cnt-4811e3c1-48b8-41a8-8b08-246224301b9b.containerhub.tripleten-services.com")
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits/"
 
--- sender_stand_request.py
+++ sender_stand_request.py
@@ -9,21 +9,21 @@ def post_new_user(body):
                          headers=data.headers)
 response_post_new_user = post_new_user(data.user_body)
 response_post_new_user_json = response_post_new_user.json()
-print(response_post_new_user_json)
+
 
 #Este código obtiene el AuthToken del usuario:
 def get_new_user_token ():
     return response_post_new_user_json["authToken"]
 #Este codigo le asigna la variable auth_token a la función para extraer el valor del mismo.
 auth_token = get_new_user_token()
-print(auth_token)
+
 
 #Este código cambia el formato del authtoken extraido para luego pasarlo como headers en la solicitud de creación del kit para el usuario
 authorization = {
     "Content-Type": "application/json",
     "Authorization": f"Bearer {auth_token}"
 }
-print(authorization)
+
 
 #Este código obtiene un nuevo kit para el usuario
 def post_new_client_kit(name):
